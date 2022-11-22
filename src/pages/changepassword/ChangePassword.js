2ee8b090d12bc0f0ed4c1ab2b40ce784a946eda5
yairgyee <51312790+yairgyee@users.noreply.github.com>
2022-11-22 12:53:08 +0630

    ok

--- src/pages/changepassword/ChangePassword.js
+++ src/pages/changepassword/ChangePassword.js
@@ -1,12 +1,80 @@
-import { Button, Paper, Stack, TextField, Typography } from "@mui/material";
+import { Close } from "@mui/icons-material";
+import {
+  Alert,
+  Button,
+  IconButton,
+  Paper,
+  Snackbar,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { teal } from "@mui/material/colors";
-import React from "react";
+import React, { useState } from "react";
 import BetButtonCom from "../../components/BetButtonCom";
 import BetCom from "../../components/BetCom";
+import Axios from "../../shared/Axios";
 
 const ChangePassword = () => {
+  //alert Control
+  const [alertCtl, setAlertCtl] = useState(false);
+  // password
+  const [changePass, setChangePass] = useState({
+    oldPassword: "",
+    newPassword: "",
+    confirmPassword: "",
+  });
+
+  //function
+  const changepasswordFunction = (e) => {
+    console.log(e.target.name, e.target.value);
+    const { name, value } = e.target;
+    setChangePass({ ...changePass, [name]: value });
+  };
+  // console.log(changePass);
+
+  //confirm
+  const updatePassword = (changePass) => {
+    if (changePass.newPassword.length < 7) {
+      setAlertCtl(true);
+      return;
+    } else {
+      Axios.put(`/changepassword`, changePass, {
+        headers: {
+          authorization: `Bearer ` + localStorage.getItem("access-token"),
+        },
+      })
+        .then((res) => console.log(res.data))
+        .catch((err) => console.log(err));
+    }
+  };
   return (
     <Stack alignItems={"center"} padding={1} marginTop={2}>
+      {alertCtl && (
+        <Snackbar
+          open={alertCtl}
+          anchorOrigin={{ vertical: "top", horizontal: "center" }}
+          autoHideDuration={6000}
+          onClose={() => setAlertCtl(false)}
+          // message="Error Bet"
+          action={
+            <React.Fragment>
+              <IconButton
+                size="small"
+                // aria-label="close"
+                // color="inherit"
+                onClick={() => setAlertCtl(false)}
+              >
+                <Close fontSize="small" />
+              </IconButton>
+            </React.Fragment>
+          }
+        >
+          <Alert severity={"warning"} color={"warning"}>
+            Incorrect Password
+          </Alert>
+        </Snackbar>
+      )}
       <Stack padding={1} boxShadow={1}>
         <Typography
           variant="h5"
@@ -29,6 +97,7 @@ const ChangePassword = () => {
             name="oldPassword"
             label="oldPassword"
             sx={{ bgcolor: teal[50] }}
+            onChange={(e) => changepasswordFunction(e)}
           />
         </Stack>
         <Stack spacing={4} direction={"row"} padding={1} alignItems={"center"}>
@@ -44,6 +113,7 @@ const ChangePassword = () => {
             name="newPassword"
             label="newPassword"
             sx={{ bgcolor: teal[50] }}
+            onChange={(e) => changepasswordFunction(e)}
           />
         </Stack>
         <Stack spacing={4} direction={"row"} padding={1} alignItems={"center"}>
@@ -59,6 +129,7 @@ const ChangePassword = () => {
             name="confirmPassword"
             label="confirmPassword"
             sx={{ bgcolor: teal[50] }}
+            onChange={(e) => changepasswordFunction(e)}
           />
         </Stack>
         <Stack
@@ -70,7 +141,12 @@ const ChangePassword = () => {
           <Button size="medium" color="success" variant={"contained"}>
             Cancle
           </Button>
-          <Button size="medium" color="success" variant={"contained"}>
+          <Button
+            size="medium"
+            color="success"
+            variant={"contained"}
+            onClick={() => updatePassword(changePass)}
+          >
             OK
           </Button>
         </Stack>
